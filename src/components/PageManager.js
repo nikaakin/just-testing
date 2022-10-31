1b8d4afb1826144e20eff22ed52ddc651f614aaa
nika <nikacuckiridze47@gmail.com>
2022-10-31 06:57:05 +0400

    intial commit

---
+++ src/components/PageManager.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import { FetchData } from "./FetchData";
+import FormCompoundComponent from "./FormCompoundComponent";
+import { Home } from "./Home";
+import HomePage from "./HomePage";
+import JsonFromPublic from "./JsonFromPublic";
+import { WrongPath } from "./WrongPath";
+
+export const pages = {
+  "": <Home />,
+  home: <HomePage />,
+  fetch: <FetchData />,
+  manifest: <JsonFromPublic />,
+  form: <FormCompoundComponent />,
+};
+
+export const PageManager = () => {
+  const { section } = useParams();
+
+  const path = Object.keys(pages).find((path) => path === section && path);
+
+  return <div>{path ? pages[path] : <WrongPath />}</div>;
+};
